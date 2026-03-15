f8e556bfed76dd5d42a1fb284683edf83621399d
ThrishanPoojary <thrishanpoojary208@gmail.com>
2026-03-15 21:44:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// This connects the React 'App' to the <div id="root"> in your index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
