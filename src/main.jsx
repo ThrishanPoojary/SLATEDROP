6f1c35283e52b0ec243592a2902e240c56f1ee82
ThrishanPoojary <thrishanpoojary208@gmail.com>
2026-03-15 21:53:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // ADD THIS LINE
 import App from './App.jsx'
 import './index.css'
 
+// This is the simplest, most stable way to start a React app.
+// We are injecting your 'App' into the 'root' div of your index.html.
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App in BrowserRouter here */}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
