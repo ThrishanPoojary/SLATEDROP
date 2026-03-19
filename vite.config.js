85646fb7f05826459ffcdca1cd8e2b5b414216a1
ThrishanPoojary <thrishanpoojary208@gmail.com>
2026-03-19 09:20:34 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// This tells Vite to use the React plugin to process your code
-export default defineConfig({
-  plugins: [react()],
-})
